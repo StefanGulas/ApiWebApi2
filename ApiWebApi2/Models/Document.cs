e935d821dfe451c95c3c475477a97294a00f4cb9
StefanGulas <stefan.gulas@gmail.com>
2021-03-08 08:44:44 +0100

    Projektdateien hinzufügen.

---
+++ ApiWebApi2/Models/Document.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiWebApi2.Models
+{
+    public class Document
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Größe { get; set; }
+        public string Typ { get; set; }
+        public DateTime ZeitpunktDesHochladens { get; set; }
+    }
+}
---
+++ ApiWebApi2/Models/DocumentContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiWebApi2.Models
+{
+    public class DocumentContext : DbContext
+    {
+        public DocumentContext(DbContextOptions<DocumentContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Document> Documents { get; set; }
+
+    }
+}
